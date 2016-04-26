74c785f81617c362227ae5eb58eaeace01e8e91f
THEjoezack <me@joezack.com>
2016-04-26 17:04:14 -0400

    Playing random game

--- README.md
+++ README.md
@@ -1,33 +1,4 @@
-Simple game that lets you create your own squad of chess pieces and battle it out against other custom teams!
-
-TODO:
-* Basic project setup
- * Testing
- * Browserify
- * Linting
-* Allow for "hot seat" play
- * Draggable pieces
-* Basic validation for various types of pieces (no images yet)
- * Can't move to spot same as own piece
- * Can't move if it places the king in check
-* Define conditions for winning / losing
-* Screens for creating squads, choosing opponent
-* Zany moves like castling, and 2 pawn move on first turn
-
-
----------------------------------
-| P | P | P | P | P | P | P | P |
----------------------------------
-| R | K | B | K | Q | B | K | R |
----------------------------------
-
-Configurable weights
-Ability to save "squads" and compete against other people
-
-Might have different game modes
-* Project the King
-* Project the Pawn
-* Elimination
+Simple game that lets you create your own squad of chess pieces and battle it out against other custom squads!
 
 ### Getting Started
 * npm install -g webpack
@@ -35,4 +6,17 @@ Might have different game modes
 * git clone https://github.com/THEjoezack/chess-squad.git
 * cd chess-squad
 * npm install
-* webpack ./entry.js bundle.js
+* npm start
+
+### TODO:
+* Hook up chess rules engine
+* Setup testing
+* Setup basic game of chess vs computer
+* Screens for creating squads, choosing opponent
+* Ability to save squads and compete against other people
+
+### Far future
+* Different game modes
+ * Protect the King
+ * Protect the Pawn
+ * Elimination
--- src/app.js
+++ src/app.js
@@ -2,44 +2,62 @@ require('expose?$!expose?jQuery!jquery');
 require('bootstrap-webpack');
 require('./css/chess-squad.css');
 
-var init = function(){
-    var pieces = {
-        black: {
-            '8': ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook'],
-            '7': ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
-        },
-        white: {
-            '1': ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook'],
-            '2': ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
-        }
-    };
-
-    var pieceTemplate = require('./templates/piece.hbs');
-    var maxRows = 8;
+var getDomSquares = function() {
+    var elements = [];
     var rows = $('.table.board').children('tbody').children('tr');
-    for(var color in pieces) {
-        if (pieces.hasOwnProperty(color)) {
-            for(var rowNumber in pieces[color]) {
-                if (pieces[color].hasOwnProperty(rowNumber)) {
-                    var columnNumber = 0;
-                    for(var piece in pieces[color][rowNumber]) {
-                        if (pieces[color][rowNumber].hasOwnProperty(piece)) {
-                            var row = $($(rows)[maxRows - rowNumber])[0];
-                            var column = $($(row).children('td')[columnNumber])[0];
-                            var type = pieces[color][rowNumber][piece];
-                            var html = pieceTemplate({color: color, type: type });
-                            
-                            $(column).html(html);
-                            
-                            columnNumber += 1;
-                        }
-                    }
-                }
-            }
+    for(var rowNumber = rows.length; rowNumber >= 0; rowNumber--) {
+        var row = $(rows[rowNumber])[0];
+        var td = $(row).children('td');
+        for(var tdNumber = 0; tdNumber < td.length; tdNumber++) {
+            elements.push($(td[tdNumber]));
+       }
+    }
+    return elements;
+};
+
+var pieceTemplate = require('./templates/piece.hbs');
+var drawBoard = function(squareElements, boardSquares) {
+    var pieceMap = {};
+    for(var i = 0; i < squareElements.length; i++) {
+        var model = boardSquares[i];
+        var piece = model.piece;
+        if(piece) {
+            var html = pieceTemplate({ color: model.piece.side.name, type: model.piece.type });
+            squareElements[i].html(html);
+        } else {
+            squareElements[i].html('');
         }
     }
 };
 
+var getRandomArbitrary = function(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+var makeRandomMove = function(gc, domSquares) {
+    var moves = Object.keys(gc.notatedMoves);
+    if(moves.length == 0) {
+        debugger;
+        return;
+    }
+    var index = getRandomArbitrary(0, moves.length);
+    var move = moves[index];
+
+    gc.move(move);
+    console.log(move);
+    drawBoard(domSquares, gc.game.board.squares);
+    console.log(gc);
+    
+    // bad algorithm, but temporary
+    setTimeout(function() { result = makeRandomMove(gc, domSquares) }, 100);
+};
+
+// create a game client
 $(function() {
-    init();
+    var chess = require('chess');
+    var gc = chess.create(),
+        m = null,
+        status = null;
+    var domSquares = getDomSquares(); 
+    makeRandomMove(gc, domSquares);
 });
